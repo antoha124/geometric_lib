8fa2b298be946175f2820065f089d08bffb2a1e4
Антон <anton20050915@gmail.com>
2023-09-29 15:02:17 +0300

    --

--- square.py
+++ square.py
@@ -1,14 +1,8 @@
 def area(a):
-    """
-    :param a:the length of the side of the square
-    :return: the area of the square given by the variable a
-    """
+
     return a * a
 
 
 def perimeter(a):
-    '''
-    :param a: the length of the side of the square
-    :return: the perimeter of the square given by the variable a
-    '''
+
     return 4 * a
